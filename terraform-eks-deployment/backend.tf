769096ab70eb0be3e21f9bfac792e4d07e0f0b18
aaronburton410 <aaron.burton410@gmail.com>
2024-05-29 11:03:50 +0300

    first commit

---
+++ terraform-eks-deployment/backend.tf
@@ -0,0 +1,7 @@
+terraform {
+  backend "s3" {
+    bucket = "jenkins-app-kub-2025-v2"
+    region = "us-west-1"
+    key = "eks/terraform.tfstate"
+  }
+}
---
+++ terraform-jenkins-server/backend.tf
@@ -0,0 +1,7 @@
+terraform {
+  backend "s3" {
+    bucket = "jenkins-app-kub-2025-v2"
+    region = "us-west-1"
+    key    = "jenkins-server/terraform.tfstate"
+  }
+}
